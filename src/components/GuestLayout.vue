85f68391c637bebc35ed4a6bf0b4d2af2cd7b3fd
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-06-23 13:46:12 +0630

    Update: finish GuestLayout, Login, Signup pages UI

--- src/components/GuestLayout.vue
+++ src/components/GuestLayout.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <slot></slot>
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="/src/assets/tailwind.svg"
+        alt="Your Company"
+      />
+    </div>
+
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped></style>
