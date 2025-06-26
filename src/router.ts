be39e51c60540a7f298166ee4d6c0be7d3182370
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-06-26 22:49:19 +0630

    Update: check auth user when access auth routes in router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import type { NavigationGuardNext } from 'vue-router'
 import DefaultLayout from './components/DefaultLayout.vue'
 import Login from './pages/Login.vue'
 import Home from './pages/Home.vue'
 import MyImages from './pages/MyImages.vue'
 import Signup from './pages/Signup.vue'
 import NotFound from './pages/NotFound.vue'
+import { useUserStore } from './stores/user'
 
 const routes = [
     {
@@ -13,7 +15,17 @@ const routes = [
         children: [
             { path: '/', name: 'Home', component: Home },
             { path: '/images', name: 'MyImages', component: MyImages }
-        ]
+        ],
+        beforeEnter: async (_to: any, _from: any, next: NavigationGuardNext) => {
+            try {
+                const userStore = useUserStore();
+                await userStore.fetchUser();
+                next();
+            } catch (error) {
+                // Cancel navigation if data fetching fails
+                next(false); 
+            }
+        }
     },
     {
         path: '/login',
